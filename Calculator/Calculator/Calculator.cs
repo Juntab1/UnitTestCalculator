d35124917309ded1084ba20ee7d0a49c8a9bca86
Juntab1 <jsbwhaleguy@gmail.com>
2024-07-08 14:40:40 -0700

    initial start

---
+++ Calculator/Calculator/Calculator.cs
@@ -0,0 +1,13 @@
+﻿public class Calculator 
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    public int Add(int x, int y)
+    {
+        return x + y;
+    }
+
+    // https://stackify.com/unit-testing-basics-best-practices/
+}
