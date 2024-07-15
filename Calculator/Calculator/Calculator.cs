0f8feb8825307200aef28b26675d52c1ea228e7e
Juntab1 <jsbwhaleguy@gmail.com>
2024-07-15 15:17:16 -0700

    command operations added

--- Calculator/Calculator/Calculator.cs
+++ Calculator/Calculator/Calculator.cs
@@ -4,9 +4,32 @@
     {
         Console.WriteLine("Hello, World!");
     }
+
     public int Add(int x, int y)
     {
         return x + y;
+    }
+
+    public int Subtract(int x, int y)
+    {
+        return x - y;
+    }
+
+    public int Multiply(int x, int y)
+    {
+        return x * y;
+    }
+    
+    public int Divide(int x, int y)
+    {
+        if (y != 0)
+        {
+            return x / y;
+        }
+        else
+        {
+            throw new InvalidOperationException("2nd number can't be 0!!!");
+        }
     }
 
     // https://stackify.com/unit-testing-basics-best-practices/
--- Calculator/Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ Calculator/Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d35124917309ded1084ba20ee7d0a49c8a9bca86")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
